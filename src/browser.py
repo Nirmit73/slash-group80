929ba160a732917aa78202c4d736837c60f463d8
srujan ponnur <srujanponnur@gmail.com>
2022-11-09 18:52:13 +0000

    Automated autoyapf fixes

--- src/browser.py
+++ src/browser.py
@@ -3,15 +3,18 @@ import os
 
 app = Flask(__name__, template_folder='templates')
 
+
 @app.route("/")
 def hello():
     return render_template("UI.html")
+
 
 @app.route("/", methods=['GET', 'POST'])
 def my_form_post():
     text = request.form['text']
     processed_text = text.lower()
     return (os.popen(f'python3 slash.py {processed_text}')).read()
+
 
 if __name__ == '__main__':
     """

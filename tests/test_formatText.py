570e4b7730095f07f62e4cac6a0354b9b869ba2b
cmriggs <cmriggs@ncsu.edu>
2022-11-22 11:43:20 -0500

    testing if github actions can use internet

--- tests/test_formatText.py
+++ tests/test_formatText.py
@@ -8,6 +8,8 @@ this file. If not, please write to: secheaper@gmail.com
 """
 
 import formatter
+import math
+import urllib3
 
 
 def test_formatSearchQuery():
@@ -17,11 +19,36 @@ def test_formatSearchQuery():
     assert formatter.formatSearchQuery("1 2") == "1+2"
     assert formatter.formatSearchQuery("A B") == "A+B"
     assert formatter.formatSearchQuery("ABC") == "ABC"
+    assert formatter.formatSearchQuery(" AB") == "+AB"
 
 
 def test_formatTitle():
     """
     Checks the formatTitle function
     """
-    assert formatter.formatTitle("0" * 50) == "0" * 40 + "..."
+    assert formatter.formatTitle("0" * 41) == "0" * 40 + "..."
+    assert formatter.formatTitle("0" * 40) == "0" * 40
     assert formatter.formatTitle("0" * 5) == "0" * 5
+
+
+def test_getNumbers():
+    """
+    Checks the getNumbers function
+    """
+    assert formatter.getNumbers(15) == 15
+    assert formatter.getNumbers('N.A') == -math.inf
+    assert formatter.getNumbers('$10.99') == 10.99
+    assert formatter.getNumbers('test ABC5.39ABC test') == 5.39
+    assert formatter.getNumbers('5..50') == -math.inf
+    assert formatter.getNumbers('5.50 test 5.50') == -math.inf
+
+
+def test_linkShortener():
+    """
+    Checks the linkShortener function
+    """
+    assert formatter.linkShortener(None) == "https://www.ncsu.edu/"
+    test_tiny = formatter.linkShortener("https://www.google.com/")
+    http = urllib3.PoolManager()  # Some shortened url
+    response = http.request('GET', test_tiny)
+    assert response.status == 200  # Checks site is successfully pulled

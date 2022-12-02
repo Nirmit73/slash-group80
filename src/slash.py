9bf69c8dc820c5acf25b70b27638d058d7655cdd
cmriggs <cmriggs@ncsu.edu>
2022-12-02 17:53:35 -0500

    Implemented threading

--- src/slash.py
+++ src/slash.py
@@ -30,96 +30,23 @@ def main(search_item, num_item, sort_item, order_item, email):
         sort = 'pr'
     elif sort_item == "rating":
         sort = 'ra'
-    # """
-    # Default
-    # default linkflag(args.link) == True
-    timeInitDefault = time.time()
 
-    products_amazon = scraper.searchAmazon(search_item, True)
-    products_walmart = scraper.searchWalmart(search_item, True)
-    products_target = scraper.searchTarget(search_item, True)
-    products_ebay = scraper.searcheBay(search_item, True)
-    products_costco = scraper.searchCostCo(search_item, True)
-    finalistList = []
-    finalistList.append(
-        formatter.sortList(products_amazon, sort, order_des)[:num_item])
-    finalistList.append(
-        formatter.sortList(products_walmart, sort, order_des)[:num_item])
-    finalistList.append(
-        formatter.sortList(products_target, sort, order_des)[:num_item])
-    finalistList.append(
-        formatter.sortList(products_ebay, sort, order_des)[:num_item])
-    finalistList.append(
-        formatter.sortList(products_costco, sort, order_des)[:num_item])
-
-    timeDiffDefault = time.time() - timeInitDefault
-    # """
-    """
-    # Threading:
-    timeInitThreading = time.time()
     finalistList = []
 
-    thread_amazon = threading.Thread(target=finalistList.append(
-        formatter.sortList(scraper.searchAmazon(search_item, True), sort, order_des)[:num_item]), daemon=True)
-
-    thread_walmart = threading.Thread(target=finalistList.append(
-        formatter.sortList(scraper.searchWalmart(search_item, True), sort, order_des)[:num_item]), daemon=True)
-
-    thread_target = threading.Thread(target=finalistList.append(
-        formatter.sortList(scraper.searchTarget(search_item, True), sort, order_des)[:num_item]), daemon=True)
-
-    thread_ebay = threading.Thread(target=finalistList.append(
-        formatter.sortList(scraper.searcheBay(search_item, True), sort, order_des)[:num_item]), daemon=True)
-
-    thread_costco = threading.Thread(target=finalistList.append(
-        formatter.sortList(scraper.searchCostCo(search_item, True), sort, order_des)[:num_item]), daemon=True)
-
-    timeDiffThreading = time.time() - timeInitThreading
-
-    """
-    """
-    # Concurrent Futures:
-
-    timeInitFutures = time.time()
-    finalistList = []
-
-    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-        executor.submit(finalistList.append(
-            formatter.sortList(scraper.searchAmazon(search_item, True), sort, order_des)[:num_item]))     # default linkflag(args.link) == True
-        executor.submit(finalistList.append(
-            formatter.sortList(scraper.searchWalmart(search_item, True), sort, order_des)[:num_item]))
-        executor.submit(finalistList.append(
-            formatter.sortList(scraper.searchTarget(search_item, True), sort, order_des)[:num_item]))
-        executor.submit(finalistList.append(
-            formatter.sortList(scraper.searchCostCo(search_item, True), sort, order_des)[:num_item]))
-        executor.submit(finalistList.append(
-            formatter.sortList(scraper.searcheBay(search_item, True), sort, order_des)[:num_item]))
-
-    timeDiffFutures = time.time() - timeInitFutures
-
-    """
-    """
-    # Pool:
-
-    timeInitPool = time.time()
-    finalistList = []
-
+    # List of stores to scrape
     stores = ['amazon', 'walmart', 'target', 'costco', 'ebay']
+    # Create a thread for each store
     pool = ThreadPool(len(stores))
+    # Scrape each store using the search item
     results = pool.starmap(scraper.searchStore,
                            zip(stores, itertools.repeat(search_item), itertools.repeat(True)))
+    # Appends results to final list of products
     for x in range(0, len(results)):
         finalistList.append(results[x][:num_item])
-
-    timeDiffPool = time.time() - timeInitPool
-    """
+    print(results)
     mergedResults = email_utils.alternateMerge(finalistList)
     results = formatter.sortList(mergedResults, sort, order_des)
 
-    print(f"Default: {timeDiffDefault}")
-    #print(f"Threading: {timeDiffThreading}")
-    #print(f"Concurrent Futures: {timeDiffFutures}")
-    #print(f"Pool: {timeDiffPool}")
     print()
     print()
     print(tabulate(results, headers="keys", tablefmt="github"))
@@ -132,7 +59,3 @@ def main(search_item, num_item, sort_item, order_item, email):
     print()
 
     return results
-
-
-if __name__ == "__main__":
-    main("Apple Computer", 10, "relevance", "Ascending", "")
